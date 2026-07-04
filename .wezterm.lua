34c09fb3f52c3efe34ea7f9d126041b64af6e0c4
Saurrabhh <saurabhkyadav99@gmail.com>
2026-07-04 16:06:42 +0000

    feat: update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -19,4 +19,28 @@ config.color_scheme = 'Tokyo Night'
 config.font = wezterm.font 'JetBrains Mono'
 config.hide_tab_bar_if_only_one_tab = true -- Let tmux handle the tabs
 
+config.keys = {
+    -- PASTE: CTRL+V on Windows, CMD+V on Mac
+    {
+        key = 'v',
+        mods = is_windows and 'CTRL' or 'SUPER', 
+        action = wezterm.action.PasteFrom 'Clipboard',
+    },
+    -- COPY: CTRL+SHIFT+C on Windows, CMD+C on Mac
+    {
+        key = 'c',
+        mods = is_windows and 'CTRL|SHIFT' or 'SUPER', 
+        action = wezterm.action.CopyTo 'Clipboard',
+    },
+}
+
+config.mouse_bindings = {
+    -- Bind releasing the left mouse button after a selection to copy to clipboard
+    {
+        event = { Up = { streak = 1, button = 'Left' } },
+        mods = 'NONE',
+        action = wezterm.action.CompleteSelectionOrOpenLinkAtMouseCursor 'Clipboard',
+    },
+}
+
 return config
